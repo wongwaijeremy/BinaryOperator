22a872078320d02ecf65799b78b730ba93ae1093
Idioteque <wongwaijeremy3.6@gmail.com>
2018-03-08 01:08:56 +0800

    initiate class library project, initiate interfaces

---
+++ BinaryOperator.Core/Group.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BinaryOperator.Core
+{
+    //https://antimatroid.wordpress.com/2012/04/01/abstract-algebra-in-c/
+    public interface IGroupoid<T>
+    {
+        T Operator(this T a, T b);
+    }
+
+    public interface ISemigroup<T> : IGroupoid<T>
+    {        
+    }
+
+    public interface IMonoid<T> : ISemigroup<T>
+    {
+        T Identity {get;}
+    }
+    public interface IGroup<T> : IMonoid<T>
+    {
+        T Inverse(T t);
+    }
+    public interface IAbelianGroup<T> : IGroup<T>
+    {        
+    }
+}
