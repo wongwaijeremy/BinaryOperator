e1666e6273463d4a3bee7001cf09859ff50eafe0
Idioteque <29251156+wongwaijeremy@users.noreply.github.com>
2018-03-09 01:12:38 +0800

    implement a generic Groupoid

--- BinaryOperator.Core/Group.cs
+++ BinaryOperator.Core/Group.cs
@@ -3,9 +3,19 @@
 namespace BinaryOperator.Core
 {
     //https://antimatroid.wordpress.com/2012/04/01/abstract-algebra-in-c/
-    public interface IGroupoid<T>
+    public class Groupoid<T> : IClosable<T>
+        where T: class
     {
-        T Operation(T a, T b);
+        public Func<T, T, T> Operator{ get; private set; }
+        public static Groupoid<T> From(Func<T, T, T> operatorExpression)
+        {
+            return new Groupoid<T>(operatorExpression);
+        }
+        internal Groupoid(Func<T, T, T> operatorExpression)
+        {
+            Operator = operatorExpression;
+        }
+        public T Operation(T a, T b) => Operator(a, b);    
     }
 
     public interface ISemigroup<T> : IGroupoid<T>
