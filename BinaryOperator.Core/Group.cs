39176a1ee59f5bc77cefe548ab01244840225963
Idioteque <29251156+wongwaijeremy@users.noreply.github.com>
2018-03-09 01:57:41 +0800

    Semi group initiate and check valid passively

--- BinaryOperator.Core/Group.cs
+++ BinaryOperator.Core/Group.cs
@@ -6,20 +6,25 @@ namespace BinaryOperator.Core
     public class Groupoid<T> : IClosable<T>
         where T: class
     {
-        public Func<T, T, T> Operator{ get; private set; }
-        public static Groupoid<T> From(Func<T, T, T> operatorExpression)
-        {
-            return new Groupoid<T>(operatorExpression);
-        }
+        public Func<T, T, T> Operator { get; private set; }
+        public static Groupoid<T> From(Func<T, T, T> operatorExpression) => new Groupoid<T>(operatorExpression);
         internal Groupoid(Func<T, T, T> operatorExpression)
         {
             Operator = operatorExpression;
         }
-        public T Operation(T a, T b) => Operator(a, b);    
+        public T Operation(T a, T b) => Operator(a, b);
+
+        public Semigroup<T> ToSemigroup() => Semigroup<T>.From(Operator);
     }
 
-    public interface ISemigroup<T> : IGroupoid<T>
-    {        
+    public class Semigroup<T> : Groupoid<T>, IAssociable<T>
+        where T: class
+    {
+        public new static Semigroup<T> From(Func<T, T, T> operatorExpression) => new Semigroup<T>(operatorExpression);
+        internal Semigroup(Func<T, T, T> operatorExpression) : base(operatorExpression)
+        {
+        }
+        private bool IsValid(T a, T b, T c) => Operator(Operator(a , b) , c).Equals(Operator(a , Operator(b , c)));
     }
 
     public interface IMonoid<T> : ISemigroup<T>
